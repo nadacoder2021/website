e3dc2828ce64f5d25e9a34b274c77d1d98e9a2a9
Nada F <nida.chm@gmail.com>
2022-05-08 09:27:53 +0100

    Added items

--- src/containers/projects/Projects.js
+++ src/containers/projects/Projects.js
@@ -37,6 +37,15 @@ function Projects(props) {
           image={tic}
           link="https://github.com/SchoolOfCode/noughts-and-crosses-plan-room22-chirag-austyn-nada"
         ></CarouselItem>
+        <CarouselItem
+          className="gpt3__features-item1"
+          title="Tribe App"
+          text="This app was created by a team of six called “Node of Ingenuity” during our School of Code 4-week long final project.This app addresses this problem, allowing users to allocate chores to family members, specify the date a chore should be completed, and mark chores as completed.
+
+          This is an app built with a React Native and Firebase, using the Expo framework. The app has been designed to be run on iPhone 11."
+          image={tribe}
+          link="https://github.com/nadacoder2021/TribeApp1"
+        ></CarouselItem>
       </Carousel>
     </div>
   );
